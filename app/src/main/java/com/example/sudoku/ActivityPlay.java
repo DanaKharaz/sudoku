cb1c5102c861cc7c7127d828f43dc97450c70219
DanaKharaz <danakharaz@gmail.com>
2024-02-10 03:02:23 +0100

    Basics of clicking cells

--- app/src/main/java/com/example/sudoku/ActivityPlay.java
+++ app/src/main/java/com/example/sudoku/ActivityPlay.java
@@ -9,6 +9,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
+import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +18,16 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class ActivityPlay extends AppCompatActivity {
+
+    // TODO: add timer
 
     Sudoku sudoku;
     boolean[][] playable = new boolean[9][9];
+    int[][] numberPositions = new int[9][9]; // positions of each number formatted as (10*row + col + 1)
 
     int mistakes = 0;
     TextView[] hearts = new TextView[3];
@@ -36,17 +43,16 @@ public class ActivityPlay extends AppCompatActivity {
     View layoutUndo;
     TextView btnErase;
     View layoutErase;
-    TextView txtErase;
     TextView btnPencil;
     View layoutPencil;
-    TextView txtPencil;
     TextView btnHint;
     View layoutHint;
-    TextView txtHint;
 
     TextView[] numbers = new TextView[9];
-    int[] numberCounts = new int[9];
+    int[] numberCounts = {9, 9, 9, 9, 9, 9, 9, 9, 9};
     TextView[][] txtCells = new TextView[9][9];
+
+    Stack<Pair<Integer, Integer>> moveHistory = new Stack<>();
 
     @SuppressLint({"UseCompatLoadingForDrawables", "MissingInflatedId"})
     @Override
@@ -54,6 +60,7 @@ public class ActivityPlay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        // TODO:
         // create new Sudoku object if making a puzzle
         // retrieve needed Sudoku object if solving a puzzle
 
@@ -67,9 +74,11 @@ public class ActivityPlay extends AppCompatActivity {
         //int[][] grid = sudoku.getGrid(); // UI TESTS !!!
         int [][] grid = new int[9][9];
         grid[5][7] = 9;
+        grid[4][8] = 9;
+        grid[0][0] = 1;
+        sudoku = new Sudoku(grid);
 
         for (int i = 0; i < 9; i++) {
-            numberCounts[i] = 9; // default value
             for (int j = 0; j < 9; j++) {
                 txtCells[i][j].setClickable(true);
 
@@ -81,8 +90,25 @@ public class ActivityPlay extends AppCompatActivity {
 
                 if (grid[i][j] != 0) {
                     numberCounts[grid[i][j] - 1]--; // one less instance of this number left
+                    // add position of this number
+                    numberPositions[grid[i][j] - 1][8 - numberCounts[grid[i][j] - 1]] = 10*i + j + 1;
 
                     txtCells[i][j].setText(String.valueOf(grid[i][j]));
+
+                    // set onClick
+                    int finalI = i;
+                    int finalJ = j;
+                    txtCells[i][j].setOnClickListener(v -> {
+                        if (marking) {
+                            Toast.makeText(this, "Cannot use pencil on pre-filled cell", Toast.LENGTH_SHORT).show();
+                        } else if (hinting) {
+                            Toast.makeText(this, "Cannot use hint on pre-filled cell", Toast.LENGTH_SHORT).show();
+                        } else if (erasing) {
+                            Toast.makeText(this, "Cannot erase pre-filled cell", Toast.LENGTH_SHORT).show();
+                        } else {
+                            selectCell(finalI, finalJ); // highlight needed cells
+                        }
+                    });
                 } else {
                     playable[i][j] = true;
 
@@ -94,6 +120,42 @@ public class ActivityPlay extends AppCompatActivity {
                     } else {
                         txtCells[i][j].setBackground(getDrawable(R.drawable.cell_play_white));
                     }
+
+                    // set onClick
+                    int finalI = i;
+                    int finalJ = j;
+                    txtCells[i][j].setOnClickListener(v -> {
+                        if (marking) {
+                            // TODO
+                        } else if (hinting) {
+                            // TODO
+                        } else if (erasing) {
+                            int n = sudoku.getGrid()[finalI][finalJ];
+                            if (n == 0) {
+                                Toast.makeText(this, "Nothing to erase", Toast.LENGTH_SHORT).show();
+                            } else { // TODO: test
+                                sudoku.removeValue(finalI, finalJ); // fix grid
+                                for (int k = 0; k < 9; k++) { // fix numberPositions
+                                    if (numberPositions[n - 1][k] == 0) {
+                                        break;
+                                    } else if (numberPositions[n - 1][k] == 10*finalI + finalJ + 1) {
+                                        do {
+                                            try {
+                                                numberPositions[n - 1][k] = numberPositions[n - 1][k + 1];
+                                            } catch (Exception e) {
+                                                numberPositions[n - 1][k] = 0;
+                                            }
+                                            k++;
+                                        } while (k < 9 && numberPositions[n - 1][k] != 0);
+                                        break;
+                                    }
+                                }
+                                txtCells[finalI][finalJ].setText(""); // fix UI
+                            }
+                        } else {
+                            selectCell(finalI, finalJ); // highlight needed cells
+                        }
+                    });
                 }
             }
         }
@@ -111,7 +173,7 @@ public class ActivityPlay extends AppCompatActivity {
         for (int i = 0; i < 9; i++) {
             numbers[i].setText(styleBtnText(i + 1, numberCounts[i]));
             numbers[i].setOnClickListener(v -> {
-                /////////
+                // TODO
             });
         }
 
@@ -119,16 +181,33 @@ public class ActivityPlay extends AppCompatActivity {
         btnUndo = findViewById(R.id.undo);
         layoutUndo = findViewById(R.id.layoutUndo);
         btnUndo.setOnClickListener(v -> {
-            /////////////
+            if (moveHistory.size() == 0) {
+                Toast.makeText(this, "Nothing to undo", Toast.LENGTH_SHORT).show();
+            } else { // TODO: test
+                Pair<Integer, Integer> move = moveHistory.pop();
+                int n = sudoku.getGrid()[move.first][move.second];
+
+                sudoku.removeValue(move.first, move.second); // fix grid
+
+                int index = 8;
+                do {
+                    index--;
+                } while(numberPositions[n - 1][index] != 0);
+                numberPositions[n - 1][index] = 0; // fix numberPositions
+
+                txtCells[move.first][move.second].setText(""); // fix UI
+                selectCell(move.first, move.second);
+            }
         });
 
         btnErase = findViewById(R.id.erase);
         layoutErase = findViewById(R.id.layoutErase);
-        txtErase = findViewById(R.id.txtErase);
         btnErase.setOnClickListener(v -> {
             if (cellSelected) {
-                ///////
+                // TODO
             } else {
+                resetCellBackground();
+
                 // adjust button position
                 ViewGroup.MarginLayoutParams paramsUndo = (ViewGroup.MarginLayoutParams) layoutUndo.getLayoutParams();
                 ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layoutErase.getLayoutParams();
@@ -136,6 +215,19 @@ public class ActivityPlay extends AppCompatActivity {
                     params.topMargin = paramsUndo.topMargin; // now same as others
                 } else {
                     params.topMargin = 40; // now above others
+
+                    // 'un-click' the rest
+                    if (marking) {
+                        ViewGroup.MarginLayoutParams paramsPencil = (ViewGroup.MarginLayoutParams) layoutPencil.getLayoutParams();
+                        paramsPencil.topMargin = paramsUndo.topMargin;
+                        layoutPencil.setLayoutParams(paramsPencil);
+                        marking = false;
+                    } else if (hinting) {
+                        ViewGroup.MarginLayoutParams paramsHint = (ViewGroup.MarginLayoutParams) layoutHint.getLayoutParams();
+                        paramsHint.topMargin = paramsUndo.topMargin;
+                        layoutHint.setLayoutParams(paramsHint);
+                        hinting = false;
+                    }
                 }
                 erasing = !erasing;
                 layoutErase.setLayoutParams(params);
@@ -144,8 +236,9 @@ public class ActivityPlay extends AppCompatActivity {
 
         btnPencil = findViewById(R.id.pencil);
         layoutPencil = findViewById(R.id.layoutPencil);
-        txtPencil = findViewById(R.id.txtPencil);
         btnPencil.setOnClickListener(v -> {
+            resetCellBackground();
+
             // adjust button position
             ViewGroup.MarginLayoutParams paramsUndo = (ViewGroup.MarginLayoutParams) layoutUndo.getLayoutParams();
             ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layoutPencil.getLayoutParams();
@@ -153,20 +246,32 @@ public class ActivityPlay extends AppCompatActivity {
                 params.topMargin = paramsUndo.topMargin; // now same as others
             } else {
                 params.topMargin = 40; // now above others
+
+                // 'un-click' the rest
+                if (erasing) {
+                    ViewGroup.MarginLayoutParams paramsErase = (ViewGroup.MarginLayoutParams) layoutErase.getLayoutParams();
+                    paramsErase.topMargin = paramsUndo.topMargin;
+                    layoutErase.setLayoutParams(paramsErase);
+                    erasing = false;
+                } else if (hinting) {
+                    ViewGroup.MarginLayoutParams paramsHint = (ViewGroup.MarginLayoutParams) layoutHint.getLayoutParams();
+                    paramsHint.topMargin = paramsUndo.topMargin;
+                    layoutHint.setLayoutParams(paramsHint);
+                    hinting = false;
+                }
             }
             marking = !marking;
             layoutPencil.setLayoutParams(params);
-
-            /////////////
         });
 
         btnHint = findViewById(R.id.hint);
         layoutHint = findViewById(R.id.layoutHint);
-        txtHint = findViewById(R.id.txtHint);
         btnHint.setOnClickListener(v -> {
             if (cellSelected) {
-                ///////
+                // TODO
             } else {
+                resetCellBackground();
+
                 // adjust button position
                 ViewGroup.MarginLayoutParams paramsUndo = (ViewGroup.MarginLayoutParams) layoutUndo.getLayoutParams();
                 ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layoutHint.getLayoutParams();
@@ -174,6 +279,19 @@ public class ActivityPlay extends AppCompatActivity {
                     params.topMargin = paramsUndo.topMargin; // now same as others
                 } else {
                     params.topMargin = 40; // now above others
+
+                    // 'un-click' the rest
+                    if (marking) {
+                        ViewGroup.MarginLayoutParams paramsPencil = (ViewGroup.MarginLayoutParams) layoutPencil.getLayoutParams();
+                        paramsPencil.topMargin = paramsUndo.topMargin;
+                        layoutPencil.setLayoutParams(paramsPencil);
+                        marking = false;
+                    } else if (erasing) {
+                        ViewGroup.MarginLayoutParams paramsErase = (ViewGroup.MarginLayoutParams) layoutErase.getLayoutParams();
+                        paramsErase.topMargin = paramsUndo.topMargin;
+                        layoutErase.setLayoutParams(paramsErase);
+                        erasing = false;
+                    }
                 }
                 hinting = !hinting;
                 layoutHint.setLayoutParams(params);
@@ -191,8 +309,70 @@ public class ActivityPlay extends AppCompatActivity {
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
+    public void selectCell(int row, int col) {
+        resetCellBackground();
+
+        if (playable[row][col]) {
+            // TODO
+        } else {
+            // highlight same number
+            int n = sudoku.getGrid()[row][col] - 1;
+            int index = 0;
+            while (numberPositions[n][index] != 0) {
+                int i = (numberPositions[n][index] - 1)/10;
+                int j = (numberPositions[n][index] - 1)%10;
+                txtCells[i][j].setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
+                index++;
+            }
+
+            // find and highlight 'neighbours'
+            // row and column
+            for (int i = 0; i < 9; i++) {
+                // same row
+                if (playable[row][i]) {txtCells[row][i].setBackground(getDrawable(R.drawable.cell_play_highlight));}
+                else {txtCells[row][i].setBackground(getDrawable(R.drawable.cell_set_highlight));}
+                // same column
+                if (playable[i][col]) {txtCells[i][col].setBackground(getDrawable(R.drawable.cell_play_highlight));}
+                else {txtCells[i][col].setBackground(getDrawable(R.drawable.cell_set_highlight));}
+            }
+            // 3x3 box
+            int startRow = row/3*3; // determine block's lowest row index
+            int startCol = col/3*3; // determine block's lowest col index
+            for (int i = startRow; i < startRow + 3; i++) {
+                for (int j = startCol; j < startCol + 3; j++) {
+                    if (playable[i][j]) {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_play_highlight));}
+                    else {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_set_highlight));}
+                }
+            }
+
+            // highlight the cell itself
+            txtCells[row][col].setBackground(getDrawable(R.drawable.cell_set_chosen));
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void resetCellBackground() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                boolean purpleCondition = i < 3 && (j < 3 || j > 5) ||
+                                          i > 5 && (j < 3 || j > 5) ||
+                                          i > 2 && i < 6 && j > 2 && j < 6;
+                if (playable[i][j]) {
+                    if (purpleCondition) {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_play_purple));}
+                    else {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_play_white));}
+                }
+                else {
+                    if (purpleCondition) {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_set_purple));}
+                    else {txtCells[i][j].setBackground(getDrawable(R.drawable.cell_set_white));}
+                }
+                txtCells[i][j].setTypeface(Typeface.MONOSPACE, Typeface.NORMAL);
+            }
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     public void check() {
-        //////
+        // TODO: check for mistakes and highlight wring answers
 
         // adjust hearts
         for (int i = 2; i >= 3 - mistakes; i--) {
@@ -201,10 +381,13 @@ public class ActivityPlay extends AppCompatActivity {
         if (mistakes == 3) {
             gameOver();
         }
+
+        // TODO: check if solved
     }
 
     public void gameOver() {
-        ////////
+        // TODO: popup about the game
+
         Toast.makeText(this, "Game Over", Toast.LENGTH_SHORT).show();
     }
 
